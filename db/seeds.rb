be0bea09f898c58b968fb47b5f6ae5ad470f5656
fdwillis <fdwillis7@gmail.com>
2015-06-29 23:52:10 -0500

    users have_many products, seed data in, to redo seed data run 'rake db:reset' without server

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,18 @@ admin = User.create!(
               password: 'pa', 
               role: 'admin'
 )
+admin.save!
 
+users = User.all.map(&:id)
 
 5.times do
   Product.create!(
+    user_id: users.sample,
     title: Faker::Name.name,
     price: Faker::Number.number(3),
     product_image: Faker::Avatar.image,
     )
 end
+
+p "Created #{User.count} Users"
+p "Created #{Product.count} Products"
