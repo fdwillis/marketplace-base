5d426fde9d5e203b703123fcfd102b1c50bfe08c
fdwillis <fdwillis7@gmail.com>
2015-06-29 22:54:52 -0500

    attempting to fix geocoder for production

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,9 +3,9 @@ class HomeController < ApplicationController
   def home
     Keen.publish("Homepage Visits",
       { 
-        visitor_city: request.location.data["city"],
-        visitor_state: request.location.data["region_name"],
-        visitor_country: request.location.data["country_name"],
+        visitor_city: request.location.city,
+        visitor_state: request.location.region_name,
+        visitor_country: request.location.country_name,
         timestamp: DateTime.now.to_date,
         day: DateTime.now.to_date.strftime("%A, %B %d"),
         hour: DateTime.now.strftime('%H'),
