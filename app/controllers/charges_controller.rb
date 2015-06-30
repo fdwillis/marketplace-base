39ba33d903007adc15b6d5e3aa627bb1d570b2b8
fdwillis <fdwillis7@gmail.com>
2015-06-30 04:35:10 -0500

    added a 'buy now' button that works

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -4,20 +4,28 @@ class ChargesController < ApplicationController
 
   def create
     # Amount in cents
-    @amount = 500
+    @crypt = ActiveSupport::MessageEncryptor.new(ENV['SECRET_KEY_BASE'])
+    @card = @crypt.decrypt_and_verify(current_user.card_number)
 
     customer = Stripe::Customer.create(
-      :email => 'example@stripe.com',
-      :card  => params[:stripeToken]
+      email: current_user.email,
+      source: {
+        object: 'card',
+        number: @card,
+        exp_month: current_user.exp_month,
+        exp_year: current_user.exp_year,
+        cvc: current_user.cvc_number,
+       },
     )
 
+    debugger
     charge = Stripe::Charge.create(
-      :customer    => customer.id,
-      :amount      => @amount,
-      :description => 'Rails Stripe customer',
-      :currency    => 'usd'
+     customer:    customer.id,
+     amount:      params[:amount].to_i,
+     description: 'Rails Stripe customer',
+     currency:    'usd'
     )
-
+    redirect_to root_path, notice: "Thanks for the purchase!"
   rescue Stripe::CardError => e
     flash[:error] = e.message
     redirect_to charges_path
