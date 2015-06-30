844b96750255e8c4ce46c9d838e3fcd671bd640f
fdwillis <fdwillis7@gmail.com>
2015-06-30 07:30:46 -0500

    stripe now creates customer if user passes '.card?' method, only charges those account with passing '.card?' method

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -4,48 +4,57 @@ class ChargesController < ApplicationController
   end
 
   def create
-    if current_user.card_number?  
-      # Amount in cents
-      @crypt = ActiveSupport::MessageEncryptor.new(ENV['SECRET_KEY_BASE'])
-      @card = @crypt.decrypt_and_verify(current_user.card_number)
+    if current_user.card?
+      if !current_user.stripe_id?
+        # Amount in cents
+        @crypt = ActiveSupport::MessageEncryptor.new(ENV['SECRET_KEY_BASE'])
+        @card = @crypt.decrypt_and_verify(current_user.card_number)
 
-      customer = Stripe::Customer.create(
-        email: current_user.email,
-        source: {
-          object: 'card',
-          number: @card,
-          exp_month: current_user.exp_month,
-          exp_year: current_user.exp_year,
-          cvc: current_user.cvc_number,
-         },
-      )
+        customer = Stripe::Customer.create(
+          email: current_user.email,
+          source: {
+            object: 'card',
+            number: @card,
+            exp_month: current_user.exp_month,
+            exp_year: current_user.exp_year,
+            cvc: current_user.cvc_number,
+           },
+        )
+        current_user.update_attributes(stripe_id: customer.id)
+        current_user.save!
 
-      charge = Stripe::Charge.create(
-       customer:    customer.id,
-       amount:      params[:price].to_i,
-       description: 'Rails Stripe customer',
-       currency:    'usd'
-      )
-      #Track this event through Keen
-      merchant60 = (params[:price].to_i * 60) / 100
-      admin40 = params[:price].to_i - merchant60
-      
-      merchant = User.find(params[:merchant_id])
-      merchant.pending_payment += merchant60
-      merchant.save!
-
-      admin = User.find_by(role: "admin")
-      admin.pending_payment += admin40
-      admin.save!
-
-      Purchase.create(title: params[:title], price: params[:price], user_id: current_user.id, product_id: params[:product_id], product_image: params[:product_image])
-      redirect_to root_path, notice: "Thanks for the purchase!"
+        charge = Stripe::Charge.create(
+         customer:    current_user.stripe_id,
+         amount:      params[:price].to_i,
+         description: 'Rails Stripe customer',
+         currency:    'usd'
+        )
+        redirect_to root_path, notice: "Thanks for the purchase!"
+      else
+        charge = Stripe::Charge.create(
+         customer:    current_user.stripe_id,
+         amount:      params[:price].to_i,
+         description: 'Rails Stripe customer',
+         currency:    'usd'
+        )
+        redirect_to root_path, notice: "Thanks for the purchase!"
+      end
     else
       redirect_to edit_user_registration_path
       flash[:error] = "Please Add Card Details"
     end
-  rescue Stripe::CardError => e
-    flash[:error] = e.message
-    redirect_to charges_path
+    #Track this event through Keen
+    merchant60 = (params[:price].to_i * 60) / 100
+    admin40 = params[:price].to_i - merchant60
+    
+    merchant = User.find(params[:merchant_id])
+    merchant.pending_payment += merchant60
+    merchant.save!
+
+    admin = User.find_by(role: "admin")
+    admin.pending_payment += admin40
+    admin.save!
+
+    Purchase.create(title: params[:title], price: params[:price], user_id: current_user.id, product_id: params[:product_id], product_image: params[:product_image])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150630104633) do
+ActiveRecord::Schema.define(version: 20150630121103) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 20150630104633) do
     t.string   "account_number"
     t.string   "stripe_recipient_id"
     t.integer  "pending_payment",                  default: 0
+    t.string   "stripe_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
