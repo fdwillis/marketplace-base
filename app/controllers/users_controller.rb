024c665614cee7f3b81d0b79e1cbe3808a831826
fdwillis <fdwillis7@gmail.com>
2015-07-04 21:14:01 -0500

    reverted back to original user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,26 +4,28 @@ class UsersController < ApplicationController
   def update
     if current_user.update_attributes(user_params)
       @crypt = ActiveSupport::MessageEncryptor.new(ENV['SECRET_KEY_BASE'])
-
-      if params[:stripe_account_type] || params[:user][:tax_id] || params[:user][:account_number] || params[:user][:card_number] || params[:user][:routing_number]
-
+      if params[:stripe_account_type]
         current_user.update_attributes(stripe_account_type: params[:stripe_account_type])
-
+      end
+      if params[:user][:tax_id]
         ssn = @crypt.encrypt_and_sign(current_user.tax_id)
         current_user.update_attributes(tax_id: ssn)
+      end
 
-        account_number = @crypt.encrypt_and_sign(params[:user][:account_number])
+      if params[:user][:account_number]
+        account_number = @crypt.encrypt_and_sign(current_user.account_number)
         current_user.update_attributes(account_number: account_number)
-        length = params[:user][:account_number].length
-        current_user.update(merchant_last_4: params[:user][:account_number].slice((length - 4)..length))
+      end
 
+      if params[:user][:card_number]
         card_number = @crypt.encrypt_and_sign(current_user.card_number)
         current_user.update_attributes(card_number: card_number)
       end
       
       if !current_user.stripe_account_id? && current_user.merchant_ready?
 
-        # Track with Keen
+        # @card = @crypt.decrypt_and_verify(current_user.card_number)
+
         #make account instead of customer
         #add address to merchants
         merchant = Stripe::Account.create(
@@ -66,38 +68,16 @@ class UsersController < ApplicationController
         current_user.update_attributes(stripe_account_id:  @stripe_account_id , merchant_secret_key: @merchant_secret_key, merchant_publishable_key: @merchant_publishable_key )
       end
 
-      flash[:notice] = "User information updated"
+      flash[:notice] = "User Information Updated"
       redirect_to edit_user_registration_path
     else
       flash[:error] = "Isses: #{current_user.errors.full_messages.to_sentence.titleize}"
       redirect_to edit_user_registration_path
     end
   end
-  def merchant_bank
-    debugger
-    if current_user.merchant_changed?
-      @crypt = ActiveSupport::MessageEncryptor.new(ENV['SECRET_KEY_BASE'])
-      Stripe.api_key = @crypt.decrypt_and_verify(current_user.merchant_secret_key)
-      merchant_account = Stripe::Account.retrieve(@crypt.decrypt_and_verify(current_user.stripe_account_id))
 
-      bank_token = Stripe::Token.create(
-          bank_account: {
-          country: "US",
-          routing_number: current_user.routing_number,
-          account_number: current_user.account_number,
-        },
-      )
-       merchant_account.external_accounts.create({external_account: bank_token.id})
-    end
-
-    Stripe.api_key = Rails.configuration.stripe[:secret_key]
-  end
-  def changed
-    debugger
-    current_user.merchant_changed?
-  end
 private
   def user_params
-     params.require(:user).permit(:merchant_last_4, :stripe_account_type, :dob_day, :dob_month, :dob_year, :first_name, :last_name, :statement_descriptor, :support_url, :support_phone, :support_email, :business_url, :merchant_id, :business_name, :stripe_recipient_id, :name, :username, :legal_name, :card_number, :exp_month, :exp_year, :cvc_number, :tax_id, :account_number, :routing_number)
+     params.require(:user).permit(:stripe_account_type, :dob_day, :dob_month, :dob_year, :first_name, :last_name, :statement_descriptor, :support_url, :support_phone, :support_email, :business_url, :merchant_id, :business_name, :stripe_recipient_id, :name, :username, :legal_name, :card_number, :exp_month, :exp_year, :cvc_number, :tax_id, :account_number, :routing_number)
   end
 end
