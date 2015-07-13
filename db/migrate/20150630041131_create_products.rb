1e02592207893e76f5254bcd77fa5277671132f9
fdwillis <fdwillis7@gmail.com>
2015-07-13 12:12:52 -0500

    users have a tax rate that should be applied to the total cost.  Make the cost into a modal that breaks down the total payment details

--- db/migrate/20150630041131_create_products.rb
+++ db/migrate/20150630041131_create_products.rb
@@ -2,7 +2,7 @@ class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
       t.string :title
-      t.decimal :price, precision: 12, scale: 3
+      t.decimal :price, precision: 12, scale: 2
       t.belongs_to :user, index: true
 
       t.timestamps null: false
--- db/migrate/20150713034824_add_tax_rate_to_users.rb
+++ db/migrate/20150713034824_add_tax_rate_to_users.rb
@@ -1,5 +1,5 @@
 class AddTaxRateToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :tax_rate, :decimal, precision: 12, scale: 3
+    add_column :users, :tax_rate, :decimal, precision: 12, scale: 2
   end
 end
