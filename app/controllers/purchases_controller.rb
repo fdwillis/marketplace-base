92d9236110868f7148c2e689826da2dc3edc0c7b
fdwillis <fdwillis7@gmail.com>
2015-07-17 18:54:36 -0500

    reducing quantity by customer quantity amount, make price reflect quantity time purchase price and on refund, increase quantity by same amount

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -11,21 +11,28 @@ class PurchasesController < ApplicationController
     #Track product tags as well with Keen
     #Grab country of the card by switching to the merchant and using Stripe::Charge.retrieve(charge_id).source.country
     if !current_user.purchases.find_by(purchase_id: params[:purchase_id]).nil? && !current_user.purchases.find_by(purchase_id: params[:purchase_id]).refunded?
-      flash[:error] = "You've Already Purchased This"
       redirect_to root_path
+      flash[:error] = "You've Already Purchased This"
+      return
     else
       @crypt = ActiveSupport::MessageEncryptor.new(ENV['SECRET_KEY_BASE'])
       @price = params[:price].to_i
+      @product = Product.find_by(uuid: params[:uuid])
+      @user = User.find(@product.user_id)
 
       if current_user.card?
         @card = @crypt.decrypt_and_verify(current_user.card_number)
-        if User.find(Product.find_by(uuid: params[:uuid]).user_id).stripe_account_id
-          
-          @currency = User.find(Product.find_by(uuid: params[:uuid]).user_id).currency
-          @stripe_account_id = @crypt.decrypt_and_verify(User.find(Product.find_by(uuid: params[:uuid]).user_id).stripe_account_id)
+        if @user.stripe_account_id
+          @currency = @user.currency
+          @stripe_account_id = @crypt.decrypt_and_verify(@user.stripe_account_id)
+
           @shipping = params[:shipping_option].to_i
-          @shipping_name = Product.find_by(uuid: params[:uuid]).shipping_options.find_by(price: (params[:shipping_option].to_f/100)).title
+          @shipping_name = @product.shipping_options.find_by(price: (params[:shipping_option].to_f/100)).title
           @ship_to = params[:ship_to]
+
+          @product_q = @product.quantity
+          @quantity = params[:quantity].to_i
+          @new_q = @product_q - @quantity
         end
         begin
           @token = Stripe::Token.create(
@@ -50,20 +57,20 @@ class PurchasesController < ApplicationController
           flash[:error] = "#{e}"
           return
         end
-
         if params[:refund_agreement]
           if params[:shipping_option]
-            if Product.find_by(uuid: params[:uuid]).user.role == 'admin'
+            if @product.user.role == 'admin'
               begin
                 @charge = User.charge_for_admin(@price, @token.id, @shipping)
-                redirect_to root_path
-                flash[:notice] = "Thanks for the purchase!"
                 Purchase.create(uuid: params[:uuid], merchant_id: params[:merchant_id], stripe_charge_id: @charge.id,
                                   title: params[:title], price: params[:price],
                                   user_id: current_user.id, product_id: params[:product_id],
                                   application_fee: 0, purchase_id: SecureRandom.uuid,
-                                  status: 'Paid', shipping_option: @shipping_name, ship_to: @ship_to,
+                                  status: 'Paid', shipping_option: @shipping_name, ship_to: @ship_to, quantity: @quantity,
                   )
+                @product.update_attributes(quantity: @new_q)
+                redirect_to root_path
+                flash[:notice] = "Thanks for the purchase!"
                 return
               rescue Stripe::CardError => e
                 redirect_to edit_user_registration_path
@@ -76,17 +83,16 @@ class PurchasesController < ApplicationController
               end
             else
               begin
-                
                 @charge = User.charge_n_process(@price, @token, @stripe_account_id, @currency, @shipping)
-                
-                redirect_to root_path
-                flash[:notice] = "Thanks for the purchase!"
                 Purchase.create(uuid: params[:uuid], merchant_id: params[:merchant_id], stripe_charge_id: @charge.id,
                                   title: params[:title], price: params[:price],
                                   user_id: current_user.id, product_id: params[:product_id],
                                   application_fee: @charge.application_fee, purchase_id: SecureRandom.uuid,
-                                  status: 'Paid', shipping_option: @shipping_name, ship_to: @ship_to,
+                                  status: 'Paid', shipping_option: @shipping_name, ship_to: @ship_to, quantity: @quantity,
                   )
+                @product.update_attributes(quantity: @new_q)
+                redirect_to root_path
+                flash[:notice] = "Thanks for the purchase!"
                 return
               rescue Stripe::CardError => e
                 redirect_to edit_user_registration_path
@@ -101,10 +107,12 @@ class PurchasesController < ApplicationController
           else
             redirect_to product_path(params[:product_id])
             flash[:error] = "Please Choose A Shipping Option"
+            return
           end
         else
           redirect_to product_path(params[:product_id])
           flash[:error] = "Plese Agree To The Sellers Return Policy"
+          return
         end
       else
         redirect_to edit_user_registration_path
--- app/models/product.rb
+++ app/models/product.rb
@@ -12,7 +12,8 @@ class Product < ActiveRecord::Base
   has_many :users, through: :purchases
 
   validates_uniqueness_of :title
-  validates :price, :quantity, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }, numericality: {greater_than: 0}
+  validates :price, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }, numericality: {greater_than: 0}
+  validates_numericality_of :price, :quantity
   validates_presence_of :title, :price, :quantity, :shipping_options
 
   mount_uploader :product_image, PhotoUploader
