149d34c0cef4a6e5a1e3ab11e9004b87f230e0c5
fdwillis <fdwillis7@gmail.com>
2015-07-21 16:35:02 -0500

    fixed up the notifications controller

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -4,7 +4,7 @@ class NotificationsController < ApplicationController
   def create
     @update = params['msg']
     @purchase = Purchase.find_by(tracking_number: @update['tracking_number'])
-    @purchase.shipping_update.find_or_create_by_tag_and_message_and_checkpoint_time(tag: @update['checkpoints']['tag'], message: @update['checkpoints']['message'], checkpoint_time: @update['checkpoints']['checkpoint_time'])
+    @purchase.shipping_updates.find_or_create_by_tag_and_message_and_checkpoint_time(tag: @update['checkpoints']['tag'], message: @update['checkpoints']['message'], checkpoint_time: @update['checkpoints']['checkpoint_time'])
     render status: :ok
     render nothing: true
   end
