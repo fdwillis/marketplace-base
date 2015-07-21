5d0f174ac260850452a1f9c7998452eb7aa77715
fdwillis <fdwillis7@gmail.com>
2015-07-21 18:20:50 -0500

    removed the satus from webhook

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,7 +2,6 @@ class NotificationsController < ApplicationController
   protect_from_forgery :except => :create
 
   def create
-    render status: :ok
     @update = params['msg']
     @purchase = Purchase.find_by(tracking_number: @update['tracking_number'])
     @checkpoints = @update['checkpoints']
