0d7b81ab57c1079ea47e565252162737bf503e15
fdwillis <fdwillis7@gmail.com>
2015-07-22 18:57:04 -0500

    fixed refunds to handle based off new way of charging, reset all DB and stripe and test from scratch

--- app/controllers/refunds_controller.rb
+++ app/controllers/refunds_controller.rb
@@ -14,20 +14,30 @@ class RefundsController < ApplicationController
   def update
     #Track With Keen "refunds fullfilled"
     @product = Product.find_by(uuid: params[:uuid])
-
     purchase = Purchase.find_by(stripe_charge_id: params[:refund_id])
     @new_q = (@product.quantity + purchase.quantity)
-    purchase.update_attributes(status: "Refunded", refunded: true)
-    @product.update_attributes(quantity: @new_q)
+
     if @new_q > 0
       @product.update_attributes(status: nil)
     end
-    @crypt = ActiveSupport::MessageEncryptor.new(ENV['SECRET_KEY_BASE'])
-    Stripe.api_key = @crypt.decrypt_and_verify((@product).user.merchant_secret_key)
-    ch = Stripe::Charge.retrieve(params[:refund_id])
-    refund = ch.refunds.create(refund_application_fee: true, amount: ch.amount)
+    if Product.find_by(uuid: params[:uuid]).user.role == 'admin'
+      debugger
+      ch = Stripe::Charge.retrieve(params[:refund_id])
+      debugger
+      refund = ch.refunds.create(amount: ch.amount)
+      debugger
+    else
+      @crypt = ActiveSupport::MessageEncryptor.new(ENV['SECRET_KEY_BASE'])
+      Stripe.api_key = @crypt.decrypt_and_verify((@product).user.merchant_secret_key)
 
-    Stripe.api_key = Rails.configuration.stripe[:secret_key]
+      ch = Stripe::Charge.retrieve(params[:refund_id])
+      refund = ch.refunds.create(refund_application_fee: true, amount: ch.amount)
+
+      Stripe.api_key = Rails.configuration.stripe[:secret_key]
+    end
+
+    purchase.update_attributes(status: "Refunded", refunded: true)
+    @product.update_attributes(quantity: @new_q)
     redirect_to refunds_path, notice: "Refund Fullfilled"
   end
 end
