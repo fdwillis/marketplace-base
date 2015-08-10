7a2666ea2ad78bbbb0e433476e6689b1c04720f1
fdwillis <fdwillis7@gmail.com>
2015-08-10 06:31:48 -0500

    going to add to refund model: product_title, product_uuid, product_quantity

--- app/controllers/refunds_controller.rb
+++ app/controllers/refunds_controller.rb
@@ -8,35 +8,44 @@ class RefundsController < ApplicationController
     #Track With Keen "refund requests"
     # let merchants handle refunds
     order = Order.find_by(uuid: params[:uuid])
-    
-    if order.refunds.map(&:amount).sum + params[:amount].to_f < order.total_price
+    if order.refunds.map(&:amount).sum + params[:amount].to_f <= order.total_price
       order.refunds.create(amount: params[:amount], note: params[:note], refunded: false, uuid: SecureRandom.uuid, status: "Pending", merchant_id: order.merchant_id)
+    else
+      redirect_to orders_path
+      flash[:error] = "Your Refund Request Amount Is Too Big"
+      return
     end
     redirect_to orders_path
     flash[:alert] = "Your Refund Is Pending"
   end
   def update
     #Track With Keen "refunds fullfilled"
+    debugger
+    redirect_to refunds_path
+    return
 
-    @order = Order.find_by(uuid: params[:uuid])
-    order = Order.find_by(stripe_charge_id: params[:refund_id])
+    @amount = params[:price].to_i
 
-    if @order.user.role == 'admin'
+    @order = Order.find_by(stripe_charge_id: params[:refund_id])
+
+    if User.find(@order.merchant_id).role == 'admin'
       ch = Stripe::Charge.retrieve(params[:refund_id])
-      refund = ch.refunds.create(amount: ch.amount)
+      refund = ch.refunds.create(amount: @amount)
     else
       @crypt = ActiveSupport::MessageEncryptor.new(ENV['SECRET_KEY_BASE'])
-      Stripe.api_key = @crypt.decrypt_and_verify((User.find((@order).merchant_id)).merchant_secret_key)
+      Stripe.api_key = @crypt.decrypt_and_verify((User.find(@order.merchant_id)).merchant_secret_key)
 
       ch = Stripe::Charge.retrieve(params[:refund_id])
-      refund = ch.refunds.create(refund_application_fee: true, amount: ch.amount)
+      refund = ch.refunds.create(refund_application_fee: true, amount: @amount)
 
       Stripe.api_key = Rails.configuration.stripe[:secret_key]
     end
+
     @order.order_items.each do |oi|
       @product = Product.find_by(uuid: oi.uuid)
       @product.update_attributes(quantity: @product.quantity + oi.quantity.to_i)
     end
+
     order.update_attributes(status: "Refunded", refunded: true)
     redirect_to refunds_path, notice: "Refund Fullfilled"
   end
