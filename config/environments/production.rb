aad88e5fb77ba518db55fd5649aea75023b0f406
fdwillis <fdwillis7@gmail.com>
2015-08-19 18:14:15 -0500

    instead of having the production/dev.rb files calling Rails.application... I am calling ENV['TWILIO_AUTH_TOKEN'] explicitly

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.default_url_options = { host: 'marketplace-base.herokuapp.com'}
 
-  config.middleware.use Rack::TwilioWebhookAuthentication, Rails.configuration.twilio[:auth_token], '/voice'
+  config.middleware.use Rack::TwilioWebhookAuthentication, ENV["TWILIO_AUTH_TOKEN"] , '/voice'
 end
