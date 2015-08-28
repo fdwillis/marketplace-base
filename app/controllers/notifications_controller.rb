b0faccfbeec588b8a6d24a90f6990eb8a88a158b
fdwillis <fdwillis7@gmail.com>
2015-08-28 07:47:23 -0500

    text-to feature ready for use

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -17,34 +17,45 @@ class NotificationsController < ApplicationController
     @crypt = ActiveSupport::MessageEncryptor.new(ENV['SECRET_KEY_BASE'])
 
     text_message = params[:Body].split
-    if text_message[0].to_f.is_a? Numeric
+    
+    if text_message[0].to_f >= 1
       phone_number = params[:From][2,params[:From].length]
-      raiser_username = text_message[1].gsub(/[^a-z]/i, '').downcase
+      raiser_username = text_message[1].downcase
       stripe_amount = text_message[0].gsub(/[^0-9]/i, '').to_i
       donater = User.find_by(support_phone: phone_number)
       fundraiser = User.find_by(username: raiser_username)
+      
+      if fundraiser  
+        if donater && donater.card?
+          @token = User.new_token(donater, @crypt.decrypt_and_verify(donater.card_number))
+          if !fundraiser.admin?
 
-      if donater && donater.card?
-        @token = User.new_token(donater, @crypt.decrypt_and_verify(donater.card_number))
-        if !fundraiser.admin?
+            stripe_account_id = @crypt.decrypt_and_verify(fundraiser.stripe_account_id)
 
-          stripe_account_id = @crypt.decrypt_and_verify(fundraiser.stripe_account_id)
+            @charge = User.charge_n_process(fundraiser.merchant_secret_key, donater, stripe_amount, @token.id, stripe_account_id )
 
-          @charge = User.charge_n_process(fundraiser.merchant_secret_key, donater, stripe_amount, @token.id, stripe_account_id )
+          else
+            User.charge_for_admin(donater, stripe_amount, @token.id)
+          end
+
+          Stripe.api_key = Rails.configuration.stripe[:secret_key]
+          # Twilio message to thank user for donation
+          puts "Thanks for your $#{text_message[0]} donation to #{raiser_username}"
+          return
         else
-          User.charge_for_admin(donater, stripe_amount, @token.id)
+          # Link to enter card info and create user profile
+          puts "Please follow link to enter CC details"
+          return
         end
+      else
+        # twilio message to donater to check the username
+        puts "Please enter a valid username to donate to"
+        return
       end
     else
       #Twilio message back to donater
-    end
-
-    Stripe.api_key = Rails.configuration.stripe[:secret_key]
-
-    debugger
-    return
-    if params[:Body]
-      message = twilio_text.messages.create from: '+1(414)422-8186', to: params[:From], body: "Thanks for wanting to donate. https://www.google.com/ "
+      puts "Please enter a dollar amount first, then username of the fundraiser"
+      return
     end
   end
 end
@@ -56,4 +67,7 @@ end
 # 780995494278
 
 # Test CURL
-  # curl -X POST -d "msg[checkpoints][][message]=bar&msg[tracking_number]=1Z0F28171596013711&msg[checkpoints][][tag]=tag&msg[checkpoints][][checkpoint_time]=2014-05-02T16:24:38" http://localhost:3000/notifications
+  # Tracking
+    # curl -X POST -d "msg[checkpoints][][message]=bar&msg[tracking_number]=1Z0F28171596013711&msg[checkpoints][][tag]=tag&msg[checkpoints][][checkpoint_time]=2014-05-02T16:24:38" http://localhost:3000/notifications
+  # twilio
+    # curl -X POST -d 'Body=90.30 admin&From=+14143997341' http://localhost:3000/notifications/twilio
