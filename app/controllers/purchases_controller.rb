6acac489a4287a9e22ad807e1f2b974e5d218a2e
fdwillis <fdwillis7@gmail.com>
2015-09-02 14:48:38 -0500

    made the keen donation event work for direct donation, reworked purchase controller to work with direct donation as well. all set for direct donations via text and web

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -74,8 +74,6 @@ class PurchasesController < ApplicationController
         return
       end
     else
-      
-      @fund = FundraisingGoal.find_by(uuid: params[:uuid])
       @price = params[:donate][:donation].to_i * 100
       @card = @crypt.decrypt_and_verify(current_user.card_number)
 
@@ -91,21 +89,48 @@ class PurchasesController < ApplicationController
         return
       end
 
-      if params[:donate][:donation_type] 
-        if params[:donate][:donation_type] == "One Time"
-          if params[:donate][:donation].to_i > 0
+      if params[:uuid]
+        @fund = FundraisingGoal.find_by(uuid: params[:uuid])
+        if params[:donate][:donation_type] 
+          if params[:donate][:donation_type] == "One Time"
+            if params[:donate][:donation].to_i > 0
+              begin
+                if @merchant.role == 'admin'
+                  @charge = User.charge_for_admin(current_user, @price, @token.id)
+                  @donation = current_user.donations.create(donation_type: 'one-time', organization: @fund.user.username, amount: @price, uuid: SecureRandom.uuid, fundraising_goal_id: @fund.id)
+                else
+                  @charge = User.charge_n_process(@merchant.merchant_secret_key, current_user, @price, @token.id, @merchant_account_id)
+                  Stripe.api_key = Rails.configuration.stripe[:secret_key]
+                  @donation = current_user.donations.create(application_fee: ((Stripe::ApplicationFee.retrieve(@charge.application_fee).amount) / 100).to_f , donation_type: 'one-time', organization: @fund.user.username, amount: @price, uuid: SecureRandom.uuid, fundraising_goal_id: @fund.id)
+                end
+                
+                @fund.increment!(:backers, by = 1)
+
+              rescue Stripe::CardError => e
+                redirect_to edit_user_registration_path
+                flash[:error] = "#{e}"
+                return
+              rescue => e
+                redirect_to edit_user_registration_path
+                flash[:error] = "#{e}"
+                return
+              end
+            else
+              redirect_to fundraising_goal_path(id: @fund.slug)
+              flash[:error] = "Please Specify A Valid Donation Amount"
+            end
+            Donation.donations_to_keen(@donation, request.remote_ip, request.location.data, 'web', false)
+          else
+            @donation_plan = DonationPlan.find_by(uuid: params[:donate][:donation_type])
             begin
               if @merchant.role == 'admin'
-                @charge = User.charge_for_admin(current_user, @price, @token.id)
-                @donation = current_user.donations.create(donation_type: 'one-time', organization: @fund.user.username, amount: @price, uuid: SecureRandom.uuid, fundraising_goal_id: @fund.id)
+                @subscription = User.subscribe_to_admin(current_user, @token.id, @donation_plan.uuid)
+                @donation = current_user.donations.create(stripe_plan_name: @subscription.plan.name, stripe_subscription_id: @donation_plan.uuid ,active: true, donation_type: 'subscription', subscription_id: @subscription.id ,organization: @fund.user.username, amount: @subscription.plan.amount, uuid: SecureRandom.uuid, fundraising_goal_id: @fund.id, fundraiser_stripe_account_id: @merchant.merchant_secret_key)
               else
-                @charge = User.charge_n_process(@merchant.merchant_secret_key, current_user, @price, @token.id, @merchant_account_id)
+                @subscription = User.subscribe_to_fundraiser(@merchant.merchant_secret_key, current_user, @token.id, @merchant_account_id, @donation_plan.uuid)
                 Stripe.api_key = Rails.configuration.stripe[:secret_key]
-                @donation = current_user.donations.create(application_fee: ((Stripe::ApplicationFee.retrieve(@charge.application_fee).amount) / 100).to_f , donation_type: 'one-time', organization: @fund.user.username, amount: @price, uuid: SecureRandom.uuid, fundraising_goal_id: @fund.id)
+                @donation = current_user.donations.create(application_fee: (@subscription.plan.amount * (@subscription.application_fee_percent / 100 ) / 100 ) , stripe_plan_name: @subscription.plan.name, stripe_subscription_id: @donation_plan.uuid ,active: true, donation_type: 'subscription', subscription_id: @subscription.id ,organization: @fund.user.username, amount: @subscription.plan.amount, uuid: SecureRandom.uuid, fundraising_goal_id: @fund.id, fundraiser_stripe_account_id: @merchant.merchant_secret_key)
               end
-              
-              @fund.increment!(:backers, by = 1)
-
             rescue Stripe::CardError => e
               redirect_to edit_user_registration_path
               flash[:error] = "#{e}"
@@ -115,43 +140,28 @@ class PurchasesController < ApplicationController
               flash[:error] = "#{e}"
               return
             end
-          else
-            redirect_to fundraising_goal_path(id: @fund.slug)
-            flash[:error] = "Please Specify A Valid Donation Amount"
           end
         else
-          @donation_plan = DonationPlan.find_by(uuid: params[:donate][:donation_type])
-          begin
-            if @merchant.role == 'admin'
-              @subscription = User.subscribe_to_admin(current_user, @token.id, @donation_plan.uuid)
-              @donation = current_user.donations.create(stripe_plan_name: @subscription.plan.name, stripe_subscription_id: @donation_plan.uuid ,active: true, donation_type: 'subscription', subscription_id: @subscription.id ,organization: @fund.user.username, amount: @subscription.plan.amount, uuid: SecureRandom.uuid, fundraising_goal_id: @fund.id, fundraiser_stripe_account_id: @merchant.merchant_secret_key)
-            else
-              @subscription = User.subscribe_to_fundraiser(@merchant.merchant_secret_key, current_user, @token.id, @merchant_account_id, @donation_plan.uuid)
-              Stripe.api_key = Rails.configuration.stripe[:secret_key]
-              @donation = current_user.donations.create(application_fee: (@subscription.plan.amount * (@subscription.application_fee_percent / 100 ) / 100 ) , stripe_plan_name: @subscription.plan.name, stripe_subscription_id: @donation_plan.uuid ,active: true, donation_type: 'subscription', subscription_id: @subscription.id ,organization: @fund.user.username, amount: @subscription.plan.amount, uuid: SecureRandom.uuid, fundraising_goal_id: @fund.id, fundraiser_stripe_account_id: @merchant.merchant_secret_key)
-            end
-          rescue Stripe::CardError => e
-            redirect_to edit_user_registration_path
-            flash[:error] = "#{e}"
-            return
-          rescue => e
-            redirect_to edit_user_registration_path
-            flash[:error] = "#{e}"
-            return
-          end
+          redirect_to fundraising_goal_path(id: @fund.slug)
+          flash[:error] = "Please Specify A Donation Type"
         end
-
-        Donation.donations_to_keen(@donation, request.remote_ip, request.location.data, 'web')
-
         @fund.increment!(:backers, by = 1)
-        redirect_to fundraising_goals_path
-        flash[:notice] = "Thanks for the donation!"
-        return
-
       else
-        redirect_to fundraising_goal_path(id: @fund.slug)
-        flash[:error] = "Please Specify A Donation Type"
+        if @merchant.admin?
+          @charge = User.charge_for_admin(current_user, @price, @token.id)
+          @donation = current_user.donations.create(donation_type: 'one-time', organization: @merchant.username, amount: @price, uuid: SecureRandom.uuid)
+        else
+          @charge = User.charge_n_process(@merchant.merchant_secret_key, current_user, @price, @token.id, @merchant_account_id)
+          Stripe.api_key = Rails.configuration.stripe[:secret_key]
+          @donation = current_user.donations.create(application_fee: ((Stripe::ApplicationFee.retrieve(@charge.application_fee).amount) / 100).to_f , donation_type: 'one-time', organization: @merchant.username, amount: @price, uuid: SecureRandom.uuid)
+        end
+        Donation.donations_to_keen(@donation, request.remote_ip, request.location.data, 'web', true)  
       end
+
+      Stripe.api_key = Rails.configuration.stripe[:secret_key]
+      redirect_to request.referrer
+      flash[:notice] = "#{@merchant.username.capitalize} Thanks You For Your Donation!"
+      return
     end
   end
 
--- app/models/donation.rb
+++ app/models/donation.rb
@@ -2,7 +2,7 @@ class Donation < ActiveRecord::Base
   belongs_to :user
   belongs_to :fundraising_goal
   protected
-	  def self.donations_to_keen(donation, ip_address, location, web_or_text)
+	  def self.donations_to_keen(donation, ip_address, location, web_or_text, true_or_false)
 	  	# One time or subscription plan
 			if web_or_text == 'text'
 				Keen.publish("Donations", {
@@ -27,34 +27,93 @@ class Donation < ActiveRecord::Base
 		      application_fee: donation.application_fee,
 		      timestamp: Time.now,
 		      donate_by: web_or_text,
+		      direct_donation?: true_or_false,
 			  })  
 		  else	
-				if !donation.application_fee.nil?  
-				  Keen.publish("Donations", {
-				    marketplace_name: "MarketplaceBase",
-				    platform_for: 'donations',
-				    donation_amount: (donation.amount / 100).to_f,
-				    donation_type: donation.donation_type,
-				    ip_address: ip_address, 
-				    customer_id: donation.user_id,
-				    fundraising_goal_uuid: donation.fundraising_goal.uuid,
-				    merchant_id: donation.fundraising_goal.user_id,
-				    customer_current_zipcode: location["zipcode"],
-			      customer_current_city: location["city"] ,
-			      customer_current_state: location["region_name"],
-			      customer_current_country: location["country_code"],
-			      customer_sign_in_count: donation.user.sign_in_count,
-			      year: Time.now.strftime("%Y").to_i,
-			      month: DateTime.now.to_date.strftime("%B"),
-			      day: Time.now.strftime("%d").to_i,
-			      day_of_week: DateTime.now.to_date.strftime("%A"),
-			      hour: Time.now.strftime("%H").to_i,
-			      minute: Time.now.strftime("%M").to_i,
-			      donation_plan_uuid: donation.stripe_subscription_id,
-			      application_fee: donation.application_fee,
-			      timestamp: Time.now,
-			      donate_by: web_or_text,
-				  })  
+				if !donation.application_fee.nil? 
+					if donation.fundraising_goal
+					  Keen.publish("Donations", {
+					    marketplace_name: "MarketplaceBase",
+					    platform_for: 'donations',
+					    donation_amount: (donation.amount / 100).to_f,
+					    donation_type: donation.donation_type,
+					    ip_address: ip_address, 
+					    customer_id: donation.user_id,
+					    fundraising_goal_uuid: donation.fundraising_goal.uuid,
+					    merchant_id: donation.fundraising_goal.user_id,
+					    customer_current_zipcode: location["zipcode"],
+				      customer_current_city: location["city"] ,
+				      customer_current_state: location["region_name"],
+				      customer_current_country: location["country_code"],
+				      customer_sign_in_count: donation.user.sign_in_count,
+				      year: Time.now.strftime("%Y").to_i,
+				      month: DateTime.now.to_date.strftime("%B"),
+				      day: Time.now.strftime("%d").to_i,
+				      day_of_week: DateTime.now.to_date.strftime("%A"),
+				      hour: Time.now.strftime("%H").to_i,
+				      minute: Time.now.strftime("%M").to_i,
+				      donation_plan_uuid: donation.stripe_subscription_id,
+				      application_fee: donation.application_fee,
+				      timestamp: Time.now,
+				      donate_by: web_or_text,
+				      direct_donation?: true_or_false,
+					  })  
+							
+						#tags on each fundraising goal
+						if !donation.fundraising_goal.tags.empty?  
+						  donation.fundraising_goal.tags.each do |tag|
+							  Keen.publish("Tags On Fundraising Goals", {
+							  	marketplace_name: "MarketplaceBase", 
+							  	platform_for: 'donations', 
+							  	tag: tag.name,
+							  	fundraising_goal_uuid: donation.fundraising_goal.uuid,
+							  	donation_amount: (donation.amount / 100).to_f,
+							  	fundraising_goal_amount: donation.fundraising_goal.goal_amount,
+							  	donation_uuid: donation.uuid,
+							  	donation_type: donation.donation_type,
+							  	})
+						  end
+						else
+						  donation.fundraising_goal.tags.each do |tag|
+							  Keen.publish("Tags On Fundraising Goals", {
+							  	marketplace_name: "MarketplaceBase", 
+							  	platform_for: 'donations', 
+							  	tag: "None",
+							  	fundraising_goal_uuid: donation.fundraising_goal.uuid,
+							  	donation_amount: (donation.amount / 100).to_f,
+							  	fundraising_goal_amount: donation.fundraising_goal.goal_amount,
+							  	donation_uuid: donation.uuid,
+							  	donation_type: donation.donation_type,
+							  	})
+						  end
+					  end
+						else
+							Keen.publish("Donations", {
+						    marketplace_name: "MarketplaceBase",
+						    platform_for: 'donations',
+						    donation_amount: (donation.amount / 100).to_f,
+						    donation_type: donation.donation_type,
+						    ip_address: ip_address, 
+						    customer_id: donation.user_id,
+						    merchant_id: User.find_by(username: donation.organization).id,
+						    customer_current_zipcode: location["zipcode"],
+					      customer_current_city: location["city"] ,
+					      customer_current_state: location["region_name"],
+					      customer_current_country: location["country_code"],
+					      customer_sign_in_count: donation.user.sign_in_count,
+					      year: Time.now.strftime("%Y").to_i,
+					      month: DateTime.now.to_date.strftime("%B"),
+					      day: Time.now.strftime("%d").to_i,
+					      day_of_week: DateTime.now.to_date.strftime("%A"),
+					      hour: Time.now.strftime("%H").to_i,
+					      minute: Time.now.strftime("%M").to_i,
+					      donation_plan_uuid: donation.stripe_subscription_id,
+					      application_fee: donation.application_fee,
+					      timestamp: Time.now,
+					      donate_by: web_or_text,
+					      direct_donation?: true_or_false,
+						  })  
+						end
 				else
 				  Keen.publish("Donations", {
 				    marketplace_name: "MarketplaceBase",
@@ -79,36 +138,9 @@ class Donation < ActiveRecord::Base
 			      donation_plan_uuid: donation.stripe_subscription_id,
 			      timestamp: Time.now,
 			      donate_by: web_or_text,
+			      direct_donation?: true_or_false,
 			    })
 				end
-				#tags on each fundraising goal
-				if !donation.fundraising_goal.tags.empty?  
-				  donation.fundraising_goal.tags.each do |tag|
-					  Keen.publish("Tags On Fundraising Goals", {
-					  	marketplace_name: "MarketplaceBase", 
-					  	platform_for: 'donations', 
-					  	tag: tag.name,
-					  	fundraising_goal_uuid: donation.fundraising_goal.uuid,
-					  	donation_amount: (donation.amount / 100).to_f,
-					  	fundraising_goal_amount: donation.fundraising_goal.goal_amount,
-					  	donation_uuid: donation.uuid,
-					  	donation_type: donation.donation_type,
-					  	})
-				  end
-				else
-				  donation.fundraising_goal.tags.each do |tag|
-					  Keen.publish("Tags On Fundraising Goals", {
-					  	marketplace_name: "MarketplaceBase", 
-					  	platform_for: 'donations', 
-					  	tag: "None",
-					  	fundraising_goal_uuid: donation.fundraising_goal.uuid,
-					  	donation_amount: (donation.amount / 100).to_f,
-					  	fundraising_goal_amount: donation.fundraising_goal.goal_amount,
-					  	donation_uuid: donation.uuid,
-					  	donation_type: donation.donation_type,
-					  	})
-				  end
-			  end
 			end
 	  end
 
